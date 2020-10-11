e04d13b9ae31313aea570429478628ae272eaab5
Mithil Poojary <mithil467@gmail.com>
2020-10-12 01:04:01 +0530

    Lint with clang-format

--- source/Main.c
+++ source/Main.c
@@ -3,42 +3,40 @@
 #include <stdlib.h>
 #include <unistd.h>
 
-int main(int argc, char **argv)
-{
-    FILE *fp;
+int main(int argc, char **argv) {
+  FILE *fp;
 
-    int window_dimensions[4], i = 0;
-    int x, y, w, h;
+  int window_dimensions[4], i = 0;
+  int x, y, w, h;
 
-    // Most screens have width less than 1000
-    // +1 for safety, +1 for \0
-    char op[6];
+  // Most screens have width less than 1000
+  // +1 for safety, +1 for \0
+  char op[6];
 
-    if (access("get_window_details.sh", F_OK) != -1)
-        fp = popen("bash get_window_details.sh", "r");
-    else
-        fp = popen("bash /usr/local/bin/get_window_details.sh", "r");
+  if (access("get_window_details.sh", F_OK) != -1)
+    fp = popen("bash get_window_details.sh", "r");
+  else
+    fp = popen("bash /usr/local/bin/get_window_details.sh", "r");
 
-    while (fgets(op, sizeof(op), fp) != NULL)
-        window_dimensions[i++] = atoi(op);
+  while (fgets(op, sizeof(op), fp) != NULL) window_dimensions[i++] = atoi(op);
 
-    gtk_init(&argc, &argv);
+  gtk_init(&argc, &argv);
 
-    GdkWindow *root = gdk_get_default_root_window();
+  GdkWindow *root = gdk_get_default_root_window();
 
-    x = window_dimensions[0];
-    y = window_dimensions[1];
-    w = window_dimensions[2];
-    h = window_dimensions[3];
+  x = window_dimensions[0];
+  y = window_dimensions[1];
+  w = window_dimensions[2];
+  h = window_dimensions[3];
 
-    // Take screenshot
-    GdkPixbuf *screenshot = gdk_pixbuf_get_from_window(root, x, y, w, h);
+  // Take screenshot
+  GdkPixbuf *screenshot = gdk_pixbuf_get_from_window(root, x, y, w, h);
 
-    // Save screenshot
-    gdk_pixbuf_save(screenshot, "/tmp/MitPick.png", "png", NULL, NULL);
+  // Save screenshot
+  gdk_pixbuf_save(screenshot, "/tmp/MitPick.png", "png", NULL, NULL);
 
-    // Copy image to clipboard
-    system("xclip -selection clipboard -t image/png -i /tmp/MitPick.png");
+  // Copy image to clipboard
+  system("xclip -selection clipboard -t image/png -i /tmp/MitPick.png");
 
-    return 0;
+  return 0;
 }
