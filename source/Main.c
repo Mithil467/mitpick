5a17d739628792f0f63e6a8ea40e4313878c2767
Mithil Poojary <mithil467@gmail.com>
2020-10-20 22:22:06 +0530

    Parse command line parameters with argp

--- source/Main.c
+++ source/Main.c
@@ -1,10 +1,20 @@
+#include <argp.h>
 #include <gtk/gtk.h>
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
 
+const char *argp_program_version = "mitpick 0.1.0";
+const char *argp_program_bug_address =
+    "https://github.com/Mithil467/mitpick/issues";
+static char description[] = "Capture the terminal from the terminal.";
+static struct argp argp = {0, 0, 0, description, 0, 0, 0};
+
 int main(int argc, char **argv) {
   FILE *fp;
+
+  argp_parse(&argp, argc, argv, 0, 0, 0);
 
   int window_dimensions[4], i = 0;
   int x, y, w, h;
@@ -19,15 +29,14 @@ int main(int argc, char **argv) {
     fp = popen("bash /usr/local/bin/get_window_details.sh", "r");
 
   while (fgets(op, sizeof(op), fp) != NULL) window_dimensions[i++] = atoi(op);
-
-  gtk_init(&argc, &argv);
-
-  GdkWindow *root = gdk_get_default_root_window();
-
   x = window_dimensions[0];
   y = window_dimensions[1];
   w = window_dimensions[2];
   h = window_dimensions[3];
+
+  gtk_init(&argc, &argv);
+
+  GdkWindow *root = gdk_get_default_root_window();
 
   // Take screenshot
   GdkPixbuf *screenshot = gdk_pixbuf_get_from_window(root, x, y, w, h);
